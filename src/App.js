bc2b46969956471853c5a67d4d92719a2613ab67
Kyle Carter <kkcarter1129@gmail.com>
2020-03-24 10:54:33 -0500

    console log

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      timer: 0
+      timer: 0,
+      word: 'hello'
     }
   }
 
   componentDidMount() {
+    //showing that the component did mount actually ran
     console.log('componentDidMount method invoked...')
     const incrementTimer = () => this.setState({ timer: this.state.timer + 1 })
     setInterval(incrementTimer, 1000)
@@ -17,7 +19,8 @@ class App extends Component {
 
 
   render() {
-    console.log(this.state.timer)
+    //Displaying what's in state in the console
+    console.log(this.state.word)
     return (
       <div className="App">
         <p>How many seconds have you been on my app?</p>
